a8d3d19845f85c0fdb26b189a79137f02555468b
Albert <albertsanchezlafuente@gmail.com>
2018-05-23 22:39:34 +0200

    Created data_download function and created get_file_path function

--- data_gathering.py
+++ data_gathering.py
@@ -2,14 +2,20 @@
 # Author: Albert Sanchez
 # May 2018
 
+from os.path import abspath, exists
+import pandas as pd
+from pandas_datareader import data
+from os import makedirs
+
 class Data_Gatherer:
     def __init__(self):
         pass
 
 
-    def download_data(self, ticker, start_time, end_time, resolution):
+    def download_data(self, ticker, category, start_time, end_time, resolution):
         """
         :param ticker: identifier of the asset (String)
+        :param category: used to divide assets in different categories (String)
         :param start_time: datetime
         :param end_time: datetime
         :param resolution: granularity of the datetime
@@ -20,7 +26,10 @@ class Data_Gatherer:
         """
         # Download price data for open, close, high, low, volume for the given ticker
         # Structure the data following the dataset structure
-        pass
+        file = self.get_file_path(ticker, category)
+        df = data.DataReader(ticker, 'morningstar', start_time, end_time)
+        df.to_csv(file)
+        return file
 
     def get_data(self, ticker, start_time, end_time, resolution):
         """
@@ -33,3 +42,16 @@ class Data_Gatherer:
         goes to the repository (directory) and loads the data into a data frame
         """
         pass
+
+    def get_file_path(self, ticker, category):
+        """
+        :param ticker: identifier of the asset (String)
+        :param category: used to divide assets in different categories (String)
+        :return: absolute path for the requested data file
+
+        """
+        folder = abspath('data/' + category)
+        if not exists(folder):
+            makedirs(folder)
+
+        return abspath(folder + '/' + ticker + '.csv')
--- main.py
+++ main.py
@@ -15,10 +15,22 @@
 # We have to decide if we treat supports and resistances as a discrete value or as a range of values and if we add a confidence interval that this is a support/resistances
 # Should we indicate in the data which are the maximum and minimum values used by our algorithm to decide that a certain price level is a supp/resistance
 
+from datetime import datetime
 from data_gathering import Data_Gatherer
 
 def main():
+    start_year = 2016
+    start_month = 1
+    start_day = 1
+    end_year = 2018
+    end_month = 1
+    end_day = 1
+
+    start_date = datetime(year = start_year, month = start_month, day = start_day)
+    end_date = datetime(year = end_year, month = end_month, day = end_day)
+
     dg = Data_Gatherer()
+    dg.download_data('AAPL','USAstocks', start_date,end_date,1)
 
 if __name__ == "__main__":
     main()
